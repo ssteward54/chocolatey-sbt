f5de22757ab1df4ff3c7483d0179e130072b63d2
Shawn Steward <ssteward@rewardsnetwork.com>
2016-01-30 13:04:37 -0600

    - Adding to github - Updated to version 0.13.9.2 - Rebuilt the install / uninstall scripts using the latest chocolatey templates to make sure the upgrade works properly - Added metadata conventions per chocolatey recommendations

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'sbt'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.bintray.com/sbt/native-packages/sbt/0.13.9/sbt-0.13.9.2.msi'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'Simple Build Tool'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
