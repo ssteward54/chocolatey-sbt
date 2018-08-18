8538612bc5bf10cac84e7963a36cae41cfc2e2e0
Jordi Garcia <jordi@improbable.io>
2018-08-19 00:29:23 +0100

    Update to 1.2.1 with uninstallscript before isntall

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'sbt'
+$softwareName = 'sbt*'
+$installerType = 'MSI'
+
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+if ($installerType -ne 'MSI') {
+  $validExitCodes = @(0)
+}
+
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://github.com/sbt/sbt/releases/download/v1.1.4/sbt-1.1.4.msi'
+$url         = 'https://github.com/sbt/sbt/releases/download/v1.2.1/sbt-1.2.1.msi'
 $url64       = ''
 
 $packageArgs = @{
@@ -14,13 +23,41 @@ $packageArgs = @{
 
   softwareName   = 'sbt*'
 
-  checksum       = '83598896F849E6C863ADA9604316075E09D96F681BAAA09F2B13D25B9A2E2238'
+  checksum       = '7F2B0450EAB084DCD1104CD6CE6B6A884DB5F994F237AA62BBC6598C727AF174'
   checksumType   = 'sha256'
   checksum64     = ''
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = "$($_.UninstallString)"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
 }
 
 Install-ChocolateyPackage @packageArgs
